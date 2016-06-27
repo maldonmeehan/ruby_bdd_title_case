161aeb1b62408c9a74d2c3618c6521ca99d7b4b6
Maldon Meehan and Noah Yasskin <maldon@>
2016-06-27 13:14:32 -0700

    add working title case method

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,8 +1,15 @@
 class String
   define_method(:title_case) do
+    preposition_array = ["A", "An", "And", "At", "But", "By", "For", "In", "Nor", "Of", "On", "Or", "So", "The", "To", "Up", "Yet"]
     split_sentence = self.split()
     split_sentence.each() do |word|
-        word.capitalize!()
+      word.downcase!()
+      word.capitalize!()
+      preposition_array.each() do |no|
+        if word.!=(split_sentence[0]) && word.==(no)
+          word.downcase!()
+        end
+      end
     end
     split_sentence.join(" ")
   end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -2,11 +2,20 @@ require('rspec')
 require('title_case')
 
 describe('String#title_case') do
-   it("capitalizes the first letter of a word") do
+   it("capitalizes the first letter of a single word") do
      expect(("beowulf").title_case()).to(eq("Beowulf"))
    end
 
    it("capitalizes the first letter of all words in a multiple word title") do
      expect(("the color purple").title_case()).to(eq("The Color Purple"))
    end
+
+   it("It does not capitalizes designated words (prepositions, conjunctions, etc") do
+     expect(("hansel And gretel").title_case()).to(eq("Hansel and Gretel"))
+   end
+
+  it("It does capitalizes designated words if they are the first word") do
+    expect(("the").title_case()).to(eq("The"))
+  end
+
 end
